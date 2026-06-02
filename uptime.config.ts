a1530648fca38d5445c541d9f9ad2da50f0411fe
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2026-06-02 17:03:19 +0200

    Add Cloudflare group with Pages monitoring

--- uptime.config.ts
+++ uptime.config.ts
@@ -4,12 +4,31 @@ export const pageConfig: PageConfig = {
   title: "Status",
   links: [{ link: 'https://github.com/marmocc/status.marmo.cc', label: 'Source' }],
   group: {
+    'Cloudflare': ['pages_portfolio', 'pages_status' ],
     'AION': ['aion_tunnel', 'aion_dashboard'],
   },
 }
 
 export const workerConfig: WorkerConfig = {
   monitors: [
+    {
+      id: 'pages_status',
+      name: 'Status',
+      method: 'GET',
+      target: 'https://status.marmo.cc',
+      timeout: 10000,
+      statusPageLink: 'https://status.marmo.cc',
+      tooltip: 'Status',
+    },
+    {
+      id: 'pages_portfolio',
+      name: 'Portfolio',
+      method: 'GET',
+      target: 'https://marmo.cc',
+      timeout: 10000,
+      statusPageLink: 'https://marmo.cc',
+      tooltip: 'Portfolio',
+    },
     {
       id: 'aion_tunnel',
       name: 'Tunnel',
@@ -23,7 +42,6 @@ export const workerConfig: WorkerConfig = {
       method: 'GET',
       target: 'https://dash.marmo.cc',
       timeout: 10000,
-
       statusPageLink: 'https://dash.marmo.cc',
       tooltip: 'Dashboard',
     },
