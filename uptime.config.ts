152c01e3c326b60a46f32e34a01cc2febe67572f
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2026-06-02 14:42:02 +0200

    Change link label to 'Source' and removed group
    Updated the link label and monitor names for clarity.

--- uptime.config.ts
+++ uptime.config.ts
@@ -3,12 +3,7 @@ import { PageConfig, WorkerConfig } from './types/config'
 // This controls the look of your public status page dashboard
 export const pageConfig: PageConfig = {
   title: "Status",
-  links: [{ link: 'https://github.com/marmocc/status.marmo.cc', label: 'GitHub' }],
-  group: {
-    'AION': {
-      monitors: ['aion_tunnel', 'aion_dashboard'],
-      expanded: true
-  },
+  links: [{ link: 'https://github.com/marmocc/status.marmo.cc', label: 'Source' }],
 }
 
 // This controls the background worker that pings your tunnel
@@ -16,14 +11,14 @@ export const workerConfig: WorkerConfig = {
   monitors: [
     {
       id: 'aion_tunnel',
-      name: 'Tunnel',
+      name: 'AION ➔ Tunnel',
       method: 'GET',
       target: 'https://tunnel.marmo.cc/ready',
       timeout: 10000,
     },
     {
       id: 'aion_dashboard',
-      name: 'Dashboard',
+      name: 'AION ➔ Dashboard',
       method: 'GET',
       target: 'https://dash.marmo.cc',
       timeout: 10000,
