61fe1079993f2c959d01b388b41fe66263c3e212
Tommaso De Tommaso <tommaso.detommaso@protonmail.com>
2026-06-02 16:31:12 +0200

    Reintroduced groups to make the status page lean

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,24 +1,25 @@
 import { PageConfig, WorkerConfig } from './types/config'
 
-// This controls the look of your public status page dashboard
 export const pageConfig: PageConfig = {
   title: "Status",
   links: [{ link: 'https://github.com/marmocc/status.marmo.cc', label: 'Source' }],
+  group: {
+    'AION': ['aion_tunnel', 'aion_dashboard'],
+  },
 }
 
-// This controls the background worker that pings your tunnel
 export const workerConfig: WorkerConfig = {
   monitors: [
     {
       id: 'aion_tunnel',
-      name: 'AION ➔ Tunnel',
+      name: 'Tunnel',
       method: 'GET',
       target: 'https://tunnel.marmo.cc/ready',
       timeout: 10000,
     },
     {
       id: 'aion_dashboard',
-      name: 'AION ➔ Dashboard',
+      name: 'Dashboard',
       method: 'GET',
       target: 'https://dash.marmo.cc',
       timeout: 10000,
